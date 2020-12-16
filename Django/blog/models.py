505131439eb4e2ded8d359bc79d7131134219bc2
shinhosuck <shinhosuck1973@gmail.com>
2020-12-16 16:12:58 +0800

    update

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+from PIL import Image
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_image = models.ImageField(upload_to="post_images")
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.post_image.path)
+        if img.width > 500 and img.height > 500:
+            new_img = (400, 400)
+            img.thumbnail(new_img)
+            img.save(self.post_image.path)
+
+    class Meta:
+        ordering = ["-date_posted"]
+
--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from blog.models import Post
 
 
 def index(request):
-    return render(request, "blog/index.html", {})
+    posts = Post.objects.all()
+    return render(request, "blog/index.html", {"posts": posts})
