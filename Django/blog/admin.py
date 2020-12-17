133583896338fccef4788d54c55eca228f3bb2cf
shinhosuck <shinhosuck1973@gmail.com>
2020-12-18 06:11:43 +0800

    update

--- Django/blog/admin.py
+++ Django/blog/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from blog.models import Post
 
 admin.site.register(Post)
+
--- Django/blog/urls.py
+++ Django/blog/urls.py
@@ -4,5 +4,6 @@ from blog.views import index
 app_name = "blog"
 
 urlpatterns = [
-    path("home/", index, name="home")
+    path("home/", index, name="home"),
+    # path("<int:user_pk>/<int:post_pk>/", likes, name="post-like")
 ]
