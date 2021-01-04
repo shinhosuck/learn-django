ec199bc76db8f97cdb2b33aaeb5a38c2b60bde31
shinhosuck <shinhosuck1973@gmail.com>
2021-01-04 20:33:41 +0800

    update

--- Django/blog/static/blog/main.js
+++ Django/blog/static/blog/main.js
@@ -1,7 +1,21 @@
-// post_detail.html
+// nav-bar
 
-const post_detail = document.querySelector(".thumbs-up-link");
-post_detail.addEventListener("click", function(event){
-    console.log(event.currentTarget);
-    event.preventDefault()
+const btnToggle = document.querySelector(".btn-toggle");
+const links = document.querySelector(".links")
+
+console.log(links.classList)
+btnToggle.addEventListener("click", function(){
+    if(links.classList.contains("show-links")){
+        links.classList.remove("show-links")
+        links.classList.add("links")
+    }else if(links.classList.contains("links")){
+        links.classList.remove("links")
+        links.classList.add("show-links")
+    }
+})
+window.addEventListener("resize", function(){
+    if(innerWidth > 700){
+        links.classList.remove("show-links")
+        links.classList.add("links")
+    }
 })
