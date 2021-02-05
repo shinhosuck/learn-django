92319dc04afb0ef7e39c0f0891c951bda556d8e5
shinhosuck <shinhosuck1973@gmail.com>
2021-02-05 19:14:40 +0800

    update

--- Django/blog/static/blog/main.js
+++ Django/blog/static/blog/main.js
@@ -1,8 +1,8 @@
+// BASE.HTML
 // nav-bar
 const btnToggle = document.querySelector(".btn-toggle");
-const links = document.querySelector(".links")
+const links = document.querySelector(".links");
 
-console.log(links.classList)
 btnToggle.addEventListener("click", function(){
     if(links.classList.contains("show-links")){
         links.classList.remove("show-links")
@@ -17,4 +17,16 @@ window.addEventListener("resize", function(){
         links.classList.remove("show-links")
         links.classList.add("links")
     }
+})
+
+const profileMainImg = document.querySelector(".profile-main-img");
+const profileSmallContainer = document.querySelector(".profile-small-container");
+
+profileMainImg.addEventListener("mouseover", function(){
+        profileSmallContainer.classList.remove("hide-div")
+        profileSmallContainer.classList.add("show-div")
+})
+profileMainImg.addEventListener("mouseleave", function(){
+        profileSmallContainer.classList.remove("show-div")
+        profileSmallContainer.classList.add("hide-div")
 })
